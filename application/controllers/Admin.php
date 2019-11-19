0b60971c8f1765ed569422695d9c02911a97c521
BOY KURNIAWAN <boykurniawan123@gmail.com>
2019-11-19 09:48:19 +0700

    ganti title

--- application/controllers/Admin.php
+++ application/controllers/Admin.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 class Admin extends MY_Controller 
 {
 
-	var $title = "siakad.ac.id";
+	var $title = "STAIN KEPULAUAN RIAU";
 
 	public function __construct()
 	{
