248430069b4d9e26b15446364031bd8931713ab8
BOY KURNIAWAN <boykurniawan123@gmail.com>
2019-11-19 09:25:30 +0700

    ganti warna bg

--- template/halaman_login/menu/menu.php
+++ template/halaman_login/menu/menu.php
@@ -1,4 +1,4 @@
- <section class="menu-section" style="padding:10px; background-color: #e6b916;">
+ <section class="menu-section" style="padding:10px; background-color: green;">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
