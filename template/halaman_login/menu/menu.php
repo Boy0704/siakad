81802a4c579db8bb7ac57ba2eadacd242ef214d1
Boy Kurniawan <boykurniawan123@gmail.com>
2019-11-24 19:35:48 +0700

    ganti login terbaru

--- template/halaman_login/menu/menu.php
+++ template/halaman_login/menu/menu.php
@@ -7,10 +7,10 @@
                     </div>
                     <div class="navbar-collapse collapse ">
                         <ul id="menu-top" class="nav navbar-nav navbar-right">
-                            <li><i class="fa fa-home"><a  href="#">Dashboard</a></i></li>
+                            <!-- <li><i class="fa fa-home"><a  href="#">Dashboard</a></i></li>
                             <li><i class="fa fa-info"><a href="#">Tentang Aplikasi</a></i></li>
                             <li><i class="fa fa-info"><a href="#">FAQ</a></i></li>
-                            <li><i class="fa fa-key"><a href="<?php echo base_url('login') ?>">Login Page</a></i></li>
+                            <li><i class="fa fa-key"><a href="<?php echo base_url('login') ?>">Login Page</a></i></li> -->
                         </ul>
                     </div>
                 </div>
