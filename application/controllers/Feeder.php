2dd5948821fc90f7422975a149d466be8cb6e016
Boy Kurniawan <boykurniawan123@gmail.com>
2020-03-23 16:24:07 +0700

    import krs dan khs feeder

--- application/controllers/Feeder.php
+++ application/controllers/Feeder.php
@@ -4,7 +4,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 /**
  * 
  */
-class Fedeer extends CI_Controller
+class Feeder extends CI_Controller
 {
 	
 	public function import_krs_feeder()
