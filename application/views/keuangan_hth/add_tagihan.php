a562b4c8809c7d13c3b95bb6a90d766526efe473
Boy Kurniawan <boykurniawan123@gmail.com>
2020-06-10 15:19:16 +0700

    up add tagihan

--- application/views/keuangan_hth/add_tagihan.php
+++ application/views/keuangan_hth/add_tagihan.php
@@ -47,51 +47,7 @@
 			<!-- <button type="button" class="btn btn-info btn-sm" data-toggle="modal" data-target="#myModal">Tambah Tagihan</button> -->
 
 			  <!-- Modal -->
-			  <div class="modal fade" id="myModal" role="dialog">
-			    <div class="modal-dialog">
-			    
-			      <!-- Modal content-->
-			      <div class="modal-content">
-			        <div class="modal-header">
-			          <button type="button" class="close" data-dismiss="modal">&times;</button>
-			          <h4 class="modal-title">Form Tagihan</h4>
-			        </div>
-			        <div class="modal-body">
-			          <form action="keuangan_hth/simpan_tagihan/<?php echo $prodi ?>" method="POST">
-			          	<div class="form-group">
-			          		<label>Nim</label>
-			          		<select name="nim" class="form-control select2">
-			          			<option value="">Pilih Nim</option>
-			          			<?php foreach ($this->db->get('student_mahasiswa')->result() as $n): ?>
-			          				<option value="<?php echo $n->nim ?>"><?php echo $n->nim.' - '.$n->nama ?></option>
-			          			<?php endforeach ?>
-			          		</select>
-			          	</div>
-			          	<div class="form-group">
-			          		<label>Nama</label>
-			          		<input type="text" name="nama" readonly="" class="form-control">
-			          	</div>
-			          	<div class="form-group">
-			          		<label>Semester</label>
-			          		<input type="text" name="semester" readonly="" class="form-control">
-			          	</div>
-			          	<div class="form-group">
-			          		<label>Periode</label>
-			          		<input type="text" name="periode" readonly="" class="form-control">
-			          	</div>
-			          	<div class="form-group">
-			          		<label>Total Tagihan UKT</label>
-			          		<input type="text" name="semester" class="form-control">
-			          	</div>
-			          </form>
-			        </div>
-			        <div class="modal-footer">
-			          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
-			        </div>
-			      </div>
-			      
-			    </div>
-			  </div>
+			  
 			
 			<?php 
 	        	$keuangan = $this->load->database('keuangan', TRUE);
@@ -164,3 +120,50 @@
 
   </div>
 </div>
+
+
+<div class="modal fade" id="myModal" role="dialog">
+			    <div class="modal-dialog">
+			    
+			      <!-- Modal content-->
+			      <div class="modal-content">
+			        <div class="modal-header">
+			          <button type="button" class="close" data-dismiss="modal">&times;</button>
+			          <h4 class="modal-title">Form Tagihan</h4>
+			        </div>
+			        <div class="modal-body">
+			          <form action="keuangan_hth/simpan_tagihan/<?php echo $prodi ?>" method="POST">
+			          	<div class="form-group">
+			          		<label>Nim</label>
+			          		<select name="nim" class="form-control select2">
+			          			<option value="">Pilih Nim</option>
+			          			<?php foreach ($this->db->get('student_mahasiswa')->result() as $n): ?>
+			          				<option value="<?php echo $n->nim ?>"><?php echo $n->nim.' - '.$n->nama ?></option>
+			          			<?php endforeach ?>
+			          		</select>
+			          	</div>
+			          	<div class="form-group">
+			          		<label>Nama</label>
+			          		<input type="text" name="nama" readonly="" class="form-control">
+			          	</div>
+			          	<div class="form-group">
+			          		<label>Semester</label>
+			          		<input type="text" name="semester" readonly="" class="form-control">
+			          	</div>
+			          	<div class="form-group">
+			          		<label>Periode</label>
+			          		<input type="text" name="periode" readonly="" class="form-control">
+			          	</div>
+			          	<div class="form-group">
+			          		<label>Total Tagihan UKT</label>
+			          		<input type="text" name="semester" class="form-control">
+			          	</div>
+			          </form>
+			        </div>
+			        <div class="modal-footer">
+			          <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+			        </div>
+			      </div>
+			      
+			    </div>
+			  </div>
